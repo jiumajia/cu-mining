5bc63905fe0766e2d8d71599ac4878696c97b826
miaoluzeng@exingcai.com <miaoluzeng@exingcai.com>
2017-09-26 16:19:25 +0800

    refactor cudata

--- data_handle/table_from_mysql.py
+++ data_handle/table_from_mysql.py
@@ -20,35 +20,47 @@ ALPHA_CONNECTION = create_engine('mysql+pymysql://exingcai:uscj!@#@172.16.88.140
 #alpha_con = create_engine('mysql+pymysql://exingcai:uscj!@#@172.16.88.163:20306/alpha')
 
 
-class Cu_Data(object):
-
+class CuData(object):
     def __init__(self, table_col_map):
         self.table_col_map = table_col_map
         self.dataset = pd.DataFrame(columns=["date"])
 
-    def get_data(self, tstart='2011-01-01', tend='2017-07-30'):
-        #从mysql数据库 读取数据
+    def get_data(self, start_date='2011-01-01', end_date='2017-07-30'):
+        read_data = self._get_data_from_mysql(start_date, end_date)
+        return read_data
+
+    def _data_to_csv(self, path="../datafiles/cu.csv"):
+        try:
+            self.dataset.to_csv(path)
+        except Exception, e:
+            raise e
+
+    def _get_data_from_mysql(self, start_date='2011-01-01', end_date='2017-07-30'):
+        # 从mysql数据库 读取数据
         for tn in self.table_col_map.keys():
             for cn in self.table_col_map[tn]:
-                sql = config.getConfig(tn, cn) % (tstart, tend)
+                sql = config.getConfig(tn, cn) % (start_date, end_date)
                 data_slice = pd.read_sql(sql, ALPHA_CONNECTION, coerce_float=True)
                 data_slice['date'] = pd.to_datetime(data_slice['date'], format='%Y-%m-%d')
-                #连接data_slice
+                # 连接data_slice
                 self.dataset = pd.merge(self.dataset, data_slice, how='outer')
 
         self.dataset = self.dataset.sort_values(by="date")
-        self.dataset.to_csv("./datafiles/cu.csv")
+
+        self._data_to_csv()
+
         return self.dataset
 
-def get_data_from_mysql(tstart,tend):
-    cu = Cu_Data(table_col_map)
-    return cu.get_data(tstart,tend)
+    @property
+    def data(self):
+        """  property of CuData """
+        return self.dataset
 
 
 if __name__ == "__main__":
-    get_data_from_mysql(tstart = '2011-01-01',tend = '2017-07-30')
-
-
+    cu = CuData(table_col_map)
+    cu.get_data(start_date='2011-01-01', end_date='2017-07-30')
+    print cu.data
 
 
 
