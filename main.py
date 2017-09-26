02c68a3d868ae1d3444e8eba880371bb04647412
miaoluzeng@exingcai.com <miaoluzeng@exingcai.com>
2017-09-26 14:34:55 +0800

    clear main function commmit

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-@date: 2017-07-25
-
-@author: zhoucuilian
-"""
 
 from feature_engineering.features_model import *
 
 if __name__ == "__main__":
 
-    tstart = '2015-01-01'
-    tend = '2017-6-20'
+    data_start_date = '2015-01-01'
+    data_end_date = '2017-6-20'
 
     # step1：读取 features_data,target_data,test_data
-    data = get_pre_data(tstart, tend)
+    data = get_pre_data(data_start_date, data_end_date)
 
-    # t1 = data.day > 5
-    # t2 = data.day < 17
-    # data = data[t1 & t2]
-
-    #run_singlemodel(train_x,train_y,test_x,test_y)
-
-    # (train_x,train_y,test_x,test_y)
-    #ensemble = ['SVM','RandomForest','Linear','DecisionTree','NaiveBayes','Bagging']
     run_model(['SVM'], data)
 
 
